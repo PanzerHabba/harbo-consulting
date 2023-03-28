b6ba24b732e82b7c69845785c538f79d1f5688de
Martin Harbo <harbo.martin@gmail.com>
2023-03-28 22:38:28 +0200

    Navbar, colors and icons

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Harbo Consulting</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Image
+          src="/harbo_logo_hori_primary.png"
+          alt='Company logo'
+          width={1000}
+          height={500}
+        />
       </main>
-
       <Footer />
     </div>
   )
